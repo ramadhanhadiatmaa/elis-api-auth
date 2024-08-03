402a33bde2e3dbe9733805a27dbdbf09c4644dc1
rsusantaelisabeth <rsusantaelisabeth@gmail.com>
2024-08-03 23:24:45 +0000

    first commit

---
+++ routes/route.go
@@ -0,0 +1,16 @@
+package routes
+
+import (
+	"auth/controllers"
+	"auth/middlewares"
+	"github.com/gofiber/fiber/v2"
+)
+
+func Route(r *fiber.App){
+	auth := r.Group("/api")
+
+	auth.Get("/:username", middlewares.Auth, controllers.Show)
+	auth.Post("/", middlewares.Auth, controllers.Create)
+	auth.Put("/:username", middlewares.Auth, controllers.Update)
+	auth.Delete("/:username", middlewares.Auth, controllers.Delete)
+}
